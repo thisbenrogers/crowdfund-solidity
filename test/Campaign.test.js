38416a19e1240b75c58175bc8dd0529f0c394a80
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-02-02 10:51:22 -0600

    Adds 2 tests
    Includes on E2E test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -63,4 +63,45 @@ describe("Campaigns", () => {
     }
   });
 
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('buy materials', '100', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('buy materials', request.description);
+  });
+
+  // * E2E
+  it ('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods
+      .createRequest('any string value', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas: '1000000' });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+
+    console.log(balance);
+    
+    assert(balance > 103);
+  });
+
 });
