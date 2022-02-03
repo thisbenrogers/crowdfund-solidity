0686db63483cb3a7f380e8883b7a832c96c83287
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-02-03 15:31:35 -0600

    Edits Contract
    Adds functions: ```getSummary()``` and ```getRequestsCount()```

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  process.env.FACTORY_ADDRESS
+  '0x9ad5d40c5c19bde4ef6894a9223cd6097744ddef'
 );
 
 export default instance;
