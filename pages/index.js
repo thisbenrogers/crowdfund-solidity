b9c5b041761c2448f49c9662fe076283904c86f5
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-02-03 09:03:37 -0600

    Renders open campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+
 import factory from '../ethereum/factory';
+
 import 'semantic-ui-css/semantic.min.css'
 
 class CampaignIndex extends Component {
@@ -10,8 +13,30 @@ class CampaignIndex extends Component {
     return { campaigns }
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <div>
+      <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+        <Button 
+          content="Create Campaign"
+          icon="add"
+          primary
+        />
+      </div>
+    );
   }
 }
 
