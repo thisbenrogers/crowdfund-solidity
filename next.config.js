efc92e23e2fd2020f7b249c97e47165fff9c0d38
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-02-02 20:25:08 -0600

    Configures nextjs

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    MNEMONIC: "sorry cotton forum sting prepare loud security chuckle course purity survey fence",
+    ENDPOINT: "https://goerli.infura.io/v3/63477657510a412ea8c536a88469fe52",
+    FACTORY_ADDRESS: "0x909d73a6379078d67a36e9c1df4503e76608f36f"
+  }
+}
